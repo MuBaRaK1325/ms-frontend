18a5311d124609a67dee50ca3d09882f059d81f3
MuBaRaK1325 <abubakarmubarak3456@gmail.com>
2026-05-15 01:46:07 +0100

    removed

--- app.js
+++ app.js
@@ -1353,7 +1353,6 @@ async function submitPin() {
 
 /* ================= ADMIN DATA LOADER ================= */
 function loadAdminData() {
-  loadWithdrawals();
   loadAdminPlans();
   loadAdminUsers();
 }
